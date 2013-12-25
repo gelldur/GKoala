b5b1d4c55e3dc0ffb53e3aa2fe5e570bbc0ffb50
Dawid Drozd <drozddawid.uam@gmail.com>
2013-12-25 23:32:31 +0100

    Initial stuff
    Adding Astyle format rules
    Adding Eclipse project generator for CMake
    Modify gitignore

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ GKoala
 ======
 
 GUI for Cocos2dx
+
+On MIT license!
+
+TODO Fill me on end
---
+++ proj.linux/generate_eclipse_project.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cmake -D CMAKE_BUILD_TYPE=Debug -D CMAKE_ECLIPSE_VERSION=4.3 -G"Eclipse CDT4 - Unix Makefiles" ../src
