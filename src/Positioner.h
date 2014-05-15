b3897a7c351f5a62a17426ca103e4345d61870db
Dawid Drozd <drozddawid.uam@gmail.com>
2014-05-15 19:17:24 +0200

    Updating Positioner from old branch

--- src/Positioner.h
+++ src/Positioner.h
@@ -11,45 +11,46 @@
 #include "cocos2d.h"
 USING_NS_CC;
 
-namespace gui
+namespace KoalaGui
 {
 
 namespace Positioner
 {
 
-CCPoint getLeftBottomCorner ( CCNode* pNode );
-CCPoint getLeftTopCorner ( CCNode* pNode );
-CCPoint getRightTopCorner ( CCNode* pNode );
-CCPoint getRightBottomCorner ( CCNode* pNode );
+CCRect getLeftBottomCorner( CCNode* pNode );
+CCRect getLeftTopCorner( CCNode* pNode );
+CCRect getRightTopCorner( CCNode* pNode );
+CCRect getRightBottomCorner( CCNode* pNode );
 
-CCPoint getCenterPoint ( CCNode* pNode );
-CCPoint getCenterPoint ( const CCSize& size );
+CCRect getCenterPoint( CCNode* pNode );
+CCPoint getCenterPoint( const CCSize& size );
 
-void setCornerToPosition ( CCNode* pNode, const CCPoint& cornerPosition,
-						   const CCPoint& position );
+void setCornerToPosition( CCNode* pNode, const CCPoint& cornerPosition,
+						  const CCPoint& position );
 
-float getScaledWidth ( CCNode* pNode );
-float getScaledHeight ( CCNode* pNode );
-CCSize getScaledSize ( CCNode* pNode );
+float getScaledWidth( CCNode* pNode );
+float getScaledHeight( CCNode* pNode );
+CCSize getScaledSize( CCNode* pNode );
 
-void centerInParent ( CCNode* pChildNode );
-void leftEdge ( CCNode* pChildNode, const float margin = 0 );
-void rightEdge ( CCNode* pChildNode, const float margin = 0 );
-void topEdge ( CCNode* pChildNode, const float margin = 0 );
-void bottomEdge ( CCNode* pChildNode, const float margin = 0 );
-void centerInParentVertically ( CCNode* pChildNode, const float margin = 0 );
-void centerInParentHorizontally ( CCNode* pChildNode, const float margin = 0 );
+void centerInParent( CCNode* pChildNode );
+void leftEdge( CCNode* pChildNode, const float margin = 0 );
+void rightEdge( CCNode* pChildNode, const float margin = 0 );
+void topEdge( CCNode* pChildNode, const float margin = 0 );
+void bottomEdge( CCNode* pChildNode, const float margin = 0 );
+void centerInParentVertically( CCNode* pChildNode, const float margin = 0 );
+void centerInParentHorizontally( CCNode* pChildNode, const float margin = 0 );
 
-void toLeftOf ( CCNode* pToLeft, CCNode* pOf, const float margin = 0 );
-void toRightOf ( CCNode* pToRight, CCNode* pOf, const float margin = 0 );
-void toAboveOf ( CCNode* pToAbove, CCNode* pOf, const float margin = 0 );
-void toBelowOf ( CCNode* pToBelow, CCNode* pOf, const float margin = 0 );
-void toCenterHorizontalOf ( CCNode* pToCenterHorizontal, CCNode* pOf,
-							const float margin = 0 );
-void toCenterVerticalOf ( CCNode* pToCenterVertical, CCNode* pOf,
-						  const float margin = 0 );
+void toLeftOf( CCNode* pToLeft, CCNode* pOf, const float margin = 0 );
+void toRightOf( CCNode* pToRight, CCNode* pOf, const float margin = 0 );
+void toAboveOf( CCNode* pToAbove, CCNode* pOf, const float margin = 0 );
+void toBelowOf( CCNode* pToBelow, CCNode* pOf, const float margin = 0 );
+void toCenterHorizontalOf( CCNode* pToCenterHorizontal, CCNode* pOf,
+						   const float margin = 0 );
+void toCenterVerticalOf( CCNode* pToCenterVertical, CCNode* pOf,
+						 const float margin = 0 );
+void toCenterOf( CCNode* pToCenter, CCNode* pOf );
 
 }
 
-} /* namespace gui */
+} /* namespace KoalaGui */
 #endif /* CCPOSITIONER_H_ */
